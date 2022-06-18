52e7e2a47efe2b0121d92b334b23ab1e48d7d046
William Rutkowski <budski82@users.noreply.github.com>
2022-06-18 13:26:53 -0400

    xzc

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ import { weather, f_temp } from "./src/weather.js";
 const getWeather = weather();
 const currentWeather = `<h2>Weather ${getWeather}</h2>`;
 const currentLocation = `<h1> ${country} | ${state} | ${city} </h1>`;
-let toFahrenheit = `<p>in the US that converts to ${f_temp}, and still chilly for June 6th.`;
+let toFahrenheit = `<p>here in the US that converts to ${f_temp}, and still chilly for June 6th.`;
 document.getElementById('app').innerHTML = `${currentLocation} ${currentWeather}<br> ${toFahrenheit}`;
