f275eec1390ba8fc2c268a8156ea8271cdad091a
William Rutkowski <budski82@users.noreply.github.com>
2022-06-18 13:24:03 -0400

    edit all and close server

--- app.js
+++ app.js
@@ -4,9 +4,5 @@ import { weather, f_temp } from "./src/weather.js";
 const getWeather = weather();
 const currentWeather = `<h2>Weather ${getWeather}</h2>`;
 const currentLocation = `<h1> ${country} | ${state} | ${city} </h1>`;
-
-//document.getElementById('app').innerHTML = `${currentLocation} ${currentWeather}`;
-console.log( `${currentLocation} ${currentWeather}`);
-console.log(f_temp);
-
-console.log('BOO World!!');
+let toFahrenheit = `<p>in the US that converts to ${f_temp}, and still chilly for June 6th.`;
+document.getElementById('app').innerHTML = `${currentLocation} ${currentWeather}<br> ${toFahrenheit}`;
--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,10 @@
+let f_temp = `To Fahrenheit = 59 degress`;
+
 export let weather =()=> {
     const temperature = "15c";
     const conditions = "Sunny";
         return temperature + " " + conditions;
 };
 
-let f_temp = `To Fahrenheit = 59 degress`;
-      
-export { f_temp}
+    export {f_temp};
 
